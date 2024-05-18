b82c306b32e5f921f8a567888e5e7c3c0d3940f9
RachelliAA <116660054+RachelliAA@users.noreply.github.com>
2024-05-18 21:44:20 +0300

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -1,18 +1,18 @@
 CREATE TABLE Forests
 (
   forest_ID INT NOT NULL,
-  forest_name INT NOT NULL,
-  location INT NOT NULL,
-  area INT NOT NULL,
+  forest_name VARCHAR(30) NOT NULL,
+  location VARCHAR(30) NOT NULL,
+  area FLOAT NOT NULL,
   PRIMARY KEY (forest_ID)
 );
 
 CREATE TABLE Research_Stations
 (
   station_ID INT NOT NULL,
-  station_name INT NOT NULL,
-  station_location INT NOT NULL,
-  research_subject INT,
+  station_name VARCHAR(30) NOT NULL,
+  station_location VARCHAR(30) NOT NULL,
+  research_subject VARCHAR(30),
   forest_ID INT NOT NULL,
   PRIMARY KEY (station_ID),
   FOREIGN KEY (forest_ID) REFERENCES Forests(forest_ID)
@@ -21,17 +21,18 @@ CREATE TABLE Research_Stations
 CREATE TABLE Forest_Fire_Prevention_Actions
 (
   action_ID INT NOT NULL,
-  action_name INT NOT NULL,
-  cost INT NOT NULL,
-  action_description INT,
+  action_name VARCHAR(30) NOT NULL,
+  cost FLOAT NOT NULL,
+  action_description VARCHAR(30),
   PRIMARY KEY (action_ID)
 );
 
-CREATE TABLE Environmental_Education_Programs
+CREATE TABLE Education_Programs
 (
   program_ID INT NOT NULL,
-  program_name INT NOT NULL,
-  program_description INT,
+  program_name VARCHAR(30) NOT NULL,
+  program_description VARCHAR(30),
+  ages INT NOT NULL,
   research_station_ID INT,
   station_ID INT NOT NULL,
   PRIMARY KEY (program_ID),
@@ -41,18 +42,17 @@ CREATE TABLE Environmental_Education_Programs
 CREATE TABLE Tree_Planting_Projects
 (
   planting_ID INT NOT NULL,
-  start_date INT NOT NULL,
-  end_date INT NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
   forest_ID INT NOT NULL,
-  donors INT,
   PRIMARY KEY (planting_ID)
 );
 
 CREATE TABLE Trees
 (
   tree_ID INT NOT NULL,
-  type INT NOT NULL,
-  origin INT NOT NULL,
+  type VARCHAR(30) NOT NULL,
+  origin VARCHAR(30) NOT NULL,
   forest_ID INT NOT NULL,
   PRIMARY KEY (tree_ID, forest_ID),
   FOREIGN KEY (forest_ID) REFERENCES Forests(forest_ID)
@@ -76,10 +76,10 @@ CREATE TABLE IsFor
   FOREIGN KEY (forest_ID) REFERENCES Forests(forest_ID)
 );
 
-CREATE TABLE Environmental_Education_Programs_ages
+CREATE TABLE Tree_Planting_Projects_donors
 (
-  ages INT NOT NULL,
-  program_ID INT NOT NULL,
-  PRIMARY KEY (ages, program_ID),
-  FOREIGN KEY (program_ID) REFERENCES Environmental_Education_Programs(program_ID)
+  donors VARCHAR(30) NOT NULL,
+  planting_ID INT NOT NULL,
+  PRIMARY KEY (donors, planting_ID),
+  FOREIGN KEY (planting_ID) REFERENCES Tree_Planting_Projects(planting_ID)
 );
