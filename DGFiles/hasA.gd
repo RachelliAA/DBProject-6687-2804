7fb72a98f62683221b5d11a2ba6070180395a54e
RachelliAA <116660054+RachelliAA@users.noreply.github.com>
2024-05-24 13:54:40 +0300

    Add files via upload
    added dg files

---
+++ DGFiles/hasA.gd
@@ -0,0 +1,32 @@
+
+[General]
+Version=1
+
+[Preferences]
+Username=
+Password=2169
+Database=
+DateFormat=
+CommitCount=0
+CommitDelay=0
+InitScript=
+
+[Table]
+Owner=FORESTS
+Name=HASA
+Count=400
+
+[Record]
+Name=FOREST_ID
+Type=NUMBER
+Size=
+Data=Random(1, 400)
+Master=
+
+[Record]
+Name=ACTION_ID
+Type=NUMBER
+Size=
+Data=Random(1, 400)
+Master=
+
---
+++ DGFiles/isFor.gd
@@ -0,0 +1,32 @@
+
+[General]
+Version=1
+
+[Preferences]
+Username=
+Password=2528
+Database=
+DateFormat=
+CommitCount=0
+CommitDelay=0
+InitScript=
+
+[Table]
+Owner=FORESTS
+Name=ISFOR
+Count=400
+
+[Record]
+Name=PLANTING_ID
+Type=NUMBER
+Size=
+Data=Random(1, 400)
+Master=
+
+[Record]
+Name=FOREST_ID
+Type=NUMBER
+Size=
+Data=Random(1, 400)
+Master=
+
