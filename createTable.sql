e311bf8fe7c4519cc3a1daac4297783d5973d407
RachelliAA <116660054+RachelliAA@users.noreply.github.com>
2024-05-24 13:14:13 +0300

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -10,9 +10,9 @@ CREATE TABLE Forests
 CREATE TABLE Research_Stations
 (
   station_ID INT NOT NULL,
-  station_name VARCHAR(30) NOT NULL,
+  station_name VARCHAR(70) NOT NULL,
   station_location VARCHAR(30) NOT NULL,
-  research_subject VARCHAR(30),
+  research_subject VARCHAR(70),
   forest_ID INT NOT NULL,
   PRIMARY KEY (station_ID),
   FOREIGN KEY (forest_ID) REFERENCES Forests(forest_ID)
@@ -21,19 +21,18 @@ CREATE TABLE Research_Stations
 CREATE TABLE Forest_Fire_Prevention_Actions
 (
   action_ID INT NOT NULL,
-  action_name VARCHAR(30) NOT NULL,
+  action_name VARCHAR(70) NOT NULL,
   cost FLOAT NOT NULL,
-  action_description VARCHAR(30),
+  action_description VARCHAR(70),
   PRIMARY KEY (action_ID)
 );
 
 CREATE TABLE Education_Programs
 (
   program_ID INT NOT NULL,
-  program_name VARCHAR(30) NOT NULL,
-  program_description VARCHAR(30),
-  ages INT NOT NULL,
-  research_station_ID INT,
+  program_name VARCHAR(70) NOT NULL,
+  program_description VARCHAR(70),
+  ages VARCHAR(30) NOT NULL,
   station_ID INT NOT NULL,
   PRIMARY KEY (program_ID),
   FOREIGN KEY (station_ID) REFERENCES Research_Stations(station_ID)
